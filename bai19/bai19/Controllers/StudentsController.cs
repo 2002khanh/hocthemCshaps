cb93587f1e99157b3fe42484f95b182994cbd130
khanh nguyen <khanhabcgh@outlook.com>
2024-06-04 10:48:44 +0700

    bai19

---
+++ bai19/bai19/Controllers/StudentsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+using System.Collections.Generic;
+using bai19.Models;
+
+namespace bai19.Controllers
+{
+    public class StudentsController : Controller
+    {
+     
+        private static List<Student> students = new List<Student>();
+
+        public IActionResult Index()
+        {
+            return View(students);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                students.Add(student);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+    
+}
+}
---
+++ bai19/bai19/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using System;
+using System.Text.RegularExpressions;
+
+
+namespace bai19.Models
+{
+      
+        public class Student
+        {
+            [Required(ErrorMessage = "Tên là bắt buộc.")]
+            [StringLength(50, ErrorMessage = "Tên không được dài quá 50 ký tự.")]
+            [RegularExpression(@"^[a-zA-Z0-9\s]+$", ErrorMessage = "Tên không được chứa ký tự đặc biệt.")]
+            [DataType(DataType.Text)]
+            public string? Name { get; set; }
+
+            [Required(ErrorMessage = "Tuổi là bắt buộc.")]
+            [Range(18, 100, ErrorMessage = "Tuổi phải từ 18 đến 100.")]
+            public int Age { get; set; }
+
+            [Required(ErrorMessage = "Email là bắt buộc.")]
+            [EmailAddress(ErrorMessage = "Email không đúng định dạng.")]
+            public string? Email { get; set; }
+        }
+    }
+
